fe3cda2834d6f881401cdeaf56024dc9bbeaf977
Administrador <admin@devops.local>
2023-09-08 08:07:04 -0300

    just moved all files to the folder called ruby. Intent to organize my repo.

--- entrada-saida - cópia.rb
+++
@@ -1,7 +0,0 @@
-#entrada e saida
-# Solicita ao usuário para inserir um nome
-print "Digite seu nome: "
-nome = gets.chomp
-
-# Imprime a mensagem de felicitações utilizando o nome inserido
-puts "Parabéns, #{nome}! Você está fazendo um ótimo trabalho!!!"
--- unless.rb
+++
@@ -1,9 +0,0 @@
-flight_status = "closed"
-
-unless flight_status == "open"
-    check_change = "can not"
-else
-    check_change = "can"
-end
-
-puts "You #{check_change} get your flight now"
